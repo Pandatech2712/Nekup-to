b01c12aabce3272763afd27735b8d54e9ee9fd26
Pandatech2712 <144889346+Pandatech2712@users.noreply.github.com>
2024-02-21 20:08:21 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,3 +15,19 @@ const product3 = {
   name: 'Anatoplavky',
   description: 'Aby muži na rozpálených plážích kromě vašeho těla obdivovali také vaše nitro.',
 };
+
+function updateProductCard(product, cardIndex) {
+  const card = document.querySelector(`.card:nth-of-type(${cardIndex})`);
+  const imageElement = card.querySelector('.card-img-top');
+  const nameElement = card.querySelector('.card-title');
+  const descriptionElement = card.querySelector('.card-text');
+
+  imageElement.src = product.image;
+  nameElement.textContent = product.name;
+  descriptionElement.textContent = product.description;
+}
+
+// Aktualizace karet pro jednotlivé produkty
+updateProductCard(product1, 1);
+updateProductCard(product2, 2);
+updateProductCard(product3, 3);
